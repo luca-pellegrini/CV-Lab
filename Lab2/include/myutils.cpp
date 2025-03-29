bd4d9c1ac5dfdbf71fe3dae979cceb77f96c7f4e
Luca Pellegrini <luca.pellegrini@disroot.org>
2025-03-29 12:34:45 +0100

    Add function print_image_details()

--- Lab2/include/myutils.cpp
+++ Lab2/include/myutils.cpp
@@ -1,6 +1,43 @@
 #include "myutils.h"
 
-#include <string>
+using std::cout;
+using std::endl;
+
+void print_image_details(const cv::Mat &img)
+{
+    std::string depth;
+    switch (img.depth()) {
+    case CV_8U:
+        depth = "CV_8U - 8-bit unsigned integers";
+        break;
+    case CV_8S:
+        depth = "CV_8S - 8-bit signed integers";
+        break;
+    case CV_16U:
+        depth = "CV_16U - 16-bit unsigned integers";
+        break;
+    case CV_16S:
+        depth = "CV_16S - 16-bit signed integers";
+        break;
+    case CV_32S:
+        depth = "CV_32S - 32-bit signed integers";
+        break;
+    case CV_32F:
+        depth = "CV_32F - 32-bit floating-point numbers";
+        break;
+    case CV_64F:
+        depth = "CV_64F - 64-bit floating-point numbers";
+        break;
+    default:
+        break;
+    }
+
+    cout << "Image details:" << endl;
+    cout << "  - Dims: " << img.rows << " x " << img.cols << endl;
+    cout << "  - Channels: " << img.channels() << endl;
+    cout << "  - Depth: " << depth << endl;
+    cout << endl;
+}
 
 std::string remove_extension_from_filename(const std::string &s)
 {
--- Lab2/include/myutils.h
+++ Lab2/include/myutils.h
@@ -1,7 +1,13 @@
 #ifndef MYUTILS_H
 #define MYUTILS_H
 
+#include <iostream>
 #include <string>
+
+#include <opencv2/core.hpp>
+
+
+void print_image_details(const cv::Mat& img);
 
 std::string remove_extension_from_filename(const std::string& s);
 
